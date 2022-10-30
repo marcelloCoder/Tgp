d7b0ec8372046ea1fdd7965d7ea53430e7130940
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-10-30 19:58:17 -0300

    Alteration

---
+++ firstjava/src/first/java/pessoa.java
@@ -0,0 +1,28 @@
+package first.java;
+
+public class pessoa {
+	
+	private String nome;
+	private String spbrenome;
+
+	public pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSpbrenome() {
+		return spbrenome;
+	}
+
+	public void setSpbrenome(String spbrenome) {
+		this.spbrenome = spbrenome;
+	}
+
+}
