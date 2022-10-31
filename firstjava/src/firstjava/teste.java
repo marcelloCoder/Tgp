7b2ebb043f910d52219ef8d984f0532abf792e02
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-10-31 14:13:43 -0300

    delete class

--- firstjava/src/firstjava/teste.java
+++
@@ -1,9 +0,0 @@
-package firstjava;
-
-public class teste {
-
-	public teste() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- firstjava/src/firstjava/teste2.java
+++
@@ -1,9 +0,0 @@
-package firstjava;
-
-public class teste2 {
-
-	public teste2() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
