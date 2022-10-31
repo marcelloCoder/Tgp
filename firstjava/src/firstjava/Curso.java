458a447818cc605bdabb16a918fa3db480ee5450
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-10-31 18:14:19 -0300

    ATT 31/10

---
+++ firstjava/src/firstjava/Curso.java
@@ -0,0 +1,24 @@
+package firstjava;
+
+public class Curso {
+
+	public static void main(String[] args) {
+		
+		double nota1 = 91.1;
+		double nota2 = 70;
+		double nota3 = 47;
+		String nome;
+		
+		double mediaFinal = (nota1 + nota2 + nota3)/3;
+		
+		if(nota1 >= 70) {
+			System.out.println("A");
+		}
+		
+		else {
+			System.out.println("B");
+		}
+
+	}
+
+}
